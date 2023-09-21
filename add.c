40b397df7ff7822d2692ca6c933e93eb8de8667a
govind singh gurjar <gurjargovind994@gmail.com>
2023-09-21 17:33:57 +0530

    c coding day1

---
+++ add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{   int num1 ;
+    printf("enter first num ; ");
+    scanf("%d" , &num1);
+    int num2 ;
+    printf("enter second  num ; ");
+    scanf("%d" , &num2);
+    int sum = num1+num2 ;
+    printf("the sum of these numbers is %d"  , sum);
+    
+    return 0;
+}
