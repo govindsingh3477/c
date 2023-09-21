232c17fb24700d65f46a9d332cd17838bd3d9a04
govind singh gurjar <gurjargovind994@gmail.com>
2023-09-21 17:06:41 +0530

    new.c

---
+++ new.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    //printf("Hello world");
+    char chr;
+    printf("enter a character : ");
+    scanf("%c",&chr);
+    printf("ascii value %d", chr);
+    return 0;
+
+    
+}
